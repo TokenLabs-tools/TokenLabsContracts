21f03f2387541437b8334a33e6872372645b36da
gonzalez962 <gonzalez96scripts@gmail.com>
2024-07-29 15:27:42 -0600

    License Comments

--- Launchpad/TokenLabsLaunchpadFactoryERC.sol
+++ Launchpad/TokenLabsLaunchpadFactoryERC.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
+/** # This code has been created by TokenLabs. # @‌website TokenLabs.network # @‌contact admin@tokenlabs.network # Its use and modification are permitted, but always give credit to TokenLabs. # This comment will help you comply with legal requirements. # # Licensed under the Apache License, Version 2.0 (the "License"); # you may not use this file except in compliance with the License. # You may obtain a copy of the License at # # http://www.apache.org/licenses/LICENSE-2.0 # # Unless required by applicable law or agreed to in writing, software # distributed under the License is distributed on an "AS IS" BASIS, # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. # See the License for the specific language governing permissions and # limitations under the License.
+*/
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
